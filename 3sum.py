cfd189ddf2f785db8ea340566dae46a96468ccd0
yw7vvAW611 <yw7vv@virginia.edu>
2021-01-24 07:21:45 -0500

    1.24 finished

--- 3sum.py
+++ 3sum.py
@@ -30,3 +30,49 @@ Constraints:
 链接：https://leetcode-cn.com/problems/3sum
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
+
+'''
+Solution 1: Brute Force
+Time Complexity O(N^3)
+Space Complexity O(1)
+'''
+
+'''
+Solution 2 double pointer
+
+Tip 不可用set，set不能add以list 为unit 的element
+
+Key：no duplicate triplet ==> solution: sort
+降低复杂度，变two sum
+
+Special Case
+1. Empty or less than 3
+2. [0,0,0,0]
+
+Time Complexity
+- SortO(nlogn)
+-find the answer O(n^2)
+
+Space Complexity 
+O(1)
+'''
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        answer = []
+        n = len(nums)
+        for first in range(n):
+            if first > 0 and nums[first - 1] == nums[first]:
+                continue
+            target = -nums[first]
+            third = n-1
+            for second in range(first+1, n):
+                if second > first+1 and nums[second] == nums[second-1]:
+                    continue
+                while second < third and nums[second] + nums[third] > target :
+                    third -= 1
+                if second == third:
+                    break
+                if nums[second] + nums[third] == target:
+                    answer.append([nums[first],nums[second],nums[third]])
+        return answer 
