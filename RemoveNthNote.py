bc0ec6c7c62ccca9361f1b1677e58dfbf0c182d3
yw7vvAW611 <yw7vv@virginia.edu>
2021-01-27 07:05:46 -0500

    1.27 Finished

--- RemoveNthNote.py
+++ RemoveNthNote.py
@@ -47,11 +47,52 @@ Space Complexity O(1)
 
 Key Dummy Node
 '''
-
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        if head.next == None:
+            return None
+        count = 1
+        cur = head
+        while cur.next != None:
+            count += 1
+            cur = cur.next
+        dummy = ListNode()
+        dummy.next = head
+        cur = dummy
+        for i in range(count - n):
+            cur = cur.next
+        cur.next= cur.next.next
+        return dummy.next
 
 '''
 Solution 2
 
 Follow up: Could you do this in one pass?
 
+快慢指针
+利用快慢指针的差，找出节点
+第一个先走n步，然后两个指针一起走直到全部走完
+
+Time Complexity：O（n）
+Space Complexity：O（1）
+
+Test Case：
+1. 中间节点
+2. 删去第一个或最后一个
 '''
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        if head.next == None:
+            return None
+        dummy = ListNode()
+        dummy.next = head
+        cur1 = dummy
+        cur2 = dummy
+        for i in range(n):
+            cur1 = cur1.next
+        while cur1.next != None:
+            cur1 = cur1.next
+            cur2 = cur2.next
+        cur2.next= cur2.next.next
+        return dummy.next
