4f29fb38492b7d517aef74e78313a1d821bcfb79
yw7vvAW611 <yw7vv@virginia.edu>
2021-02-04 06:59:39 -0500

    2.4 finished

--- UniqueBinarySearchTree.py
+++ UniqueBinarySearchTree.py
@@ -1,4 +1,54 @@
 '''
+
+96. Unique Binary Search Trees
+Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
+
+ 
+
+Example 1:
+
+
+Input: n = 3
+Output: 5
+Example 2:
+
+Input: n = 1
+Output: 1
+ 
+
+Constraints:
+
+1 <= n <= 19
+
+
+
+Solution: Dynmaic Programming
+
+Time  Complexity: O(N^2)
+Spce Complexity: O(N)
+'''
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        dp = [0] * (n+1)
+        # dp = [0 for i in range(n+1)]
+        dp[0] = 1
+        dp[1] = 1
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        for i in range(2,n + 1):
+            for j in range(i):
+                dp[i] += (dp[j] * dp[i-j-1])
+        # print(dp)
+        return dp[n]
+
+
+
+
+
+'''
 95. Unique Binary Search Trees II
 Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
 
